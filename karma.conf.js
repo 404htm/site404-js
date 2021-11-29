9fadef741221629bef30e3eaf0235f446d5c42c7
Kelly Robins <Kelly@404htm.com>
2021-11-29 01:52:13 -0500

    Removing chrome from browser list for troubleshooting

--- karma.conf.js
+++ karma.conf.js
@@ -41,6 +41,6 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
-    browsers : ['FirefoxHeadless', "Chrome"],
+    browsers : ['Firefox', "FirefoxHeadless"],
   });
 };
