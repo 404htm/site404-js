19b3ae4309de9720fbf47e7cc711ad4101241b23
Kelly Robins <Kelly@404htm.com>
2021-12-04 23:49:24 -0500

    Broke Design in ng components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostSummaryComponent } from './post-summary/post-summary.component';
+import { NavTopComponent } from './nav-top/nav-top.component';
+import { NavSideComponent } from './nav-side/nav-side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavTopComponent,
+    NavSideComponent,
+    PostSummaryComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post-summary/post-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'post-summary',
+  templateUrl: './post-summary.component.html',
+  styleUrls: ['./post-summary.component.scss']
+})
+export class PostSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
