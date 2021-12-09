1c558f127be04059027f279e56f7cb4563e68553
Kelly Robins <Kelly@404htm.com>
2021-12-09 00:06:00 -0500

    Break out scss variables

--- src/app/post-summary/post-summary.component.ts
+++ src/app/post-summary/post-summary.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'post-summary',
-  templateUrl: './post-summary.component.html'
+  templateUrl: './post-summary.component.html',
+  styleUrls: ["./post-summary.component.scss"]
 })
 export class PostSummaryComponent implements OnInit {
 
